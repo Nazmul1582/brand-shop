a2324b820e02fa42caf5bb6ee0755f6bfadd5e2d
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-18 16:03:59 +0600

    initial project and route setup

---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="font-bold text-5xl text-center mt-20">
+            <h1>This is Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
