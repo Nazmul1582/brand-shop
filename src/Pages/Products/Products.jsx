c8adcfe97694c5a1a0f95fd0e41ee41e523774aa
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-19 09:48:42 +0600

    created a products page with some child components

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,11 +1,22 @@
+import Product from "./Product";
+import Slider from "./Slider";
+
 const Products = () => {
-    return (
-        <section className="py-20">
-            <div className="container mx-auto">
-                Products
-            </div>
-        </section>
-    );
+  return (
+    <section className="py-20">
+      <div className="container mx-auto">
+        <div>
+          <Slider />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-10">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Products;
