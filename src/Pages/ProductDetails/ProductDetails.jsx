98992973f84de59c0741d0f15f43db2bc2cf3461
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-19 21:42:53 +0600

    productDetails page design completed

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -2,34 +2,37 @@ import { BiDollar } from "react-icons/bi";
 import { useLoaderData } from "react-router-dom";
 
 const ProductDetails = () => {
-    const product = useLoaderData();
-    const { name, image, brandName, type, price, rating } = product;
+  const product = useLoaderData();
+  const { name, image, brandName, type, price, rating } = product;
 
   return (
     <section className="py-20">
       <div className="container mx-auto">
-        <div className="card bg-base-100 shadow-xl">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <figure>
-            <img src={image} alt="Headphone" />
+            <img src={image} alt={`image of ${name}`} />
           </figure>
-          <div className="card-body justify-center items-center">
-            <h2 className="card-title">{name}!</h2>
-            <p>Brand: {brandName}</p>
-            <p>Category: {type}</p>
-            <div className="flex items-center">
-              Price: <BiDollar className="text-lg" /> {price}
-            </div>
-            <div className="rating my-2 mx-auto">
-              {[...Array(parseInt(rating))].map((ele, index) => (
-                <input
-                  key={index}
-                  name="rating-2"
-                  className="mask mask-star-2 bg-orange-400"
-                />
-              ))}
-            </div>
-            <div className="card-actions justify-center">
-              <button className="btn btn-info btn-outline">Update</button>
+          <div className="md:col-span-2 p-5">
+            <div className="flex flex-col justify-center h-full gap-3">
+              <h2 className="font-bold text-3xl mb-3">{name}!</h2>
+              <div className="flex items-center text-3xl font-bold">
+                <span><BiDollar className="text-3xl" /></span>
+                <span>{price}</span>
+              </div>
+              <div className="rating my-2">
+                {[...Array(parseInt(rating))].map((ele, index) => (
+                  <input
+                    key={index}
+                    name="rating-2"
+                    className="mask mask-star-2 bg-orange-400"
+                  />
+                ))}
+              </div>
+              <p>Brand: <span className="font-bold">{brandName}</span></p>
+              <p>Category: <span className="font-bold">{type}</span></p>
+              <div className="card-actions">
+                <button className="btn btn-info">Add To Cart</button>
+              </div>
             </div>
           </div>
         </div>
