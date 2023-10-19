6b8bb3afb6698c359f6ed6bb9659bb57477259ca
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-19 07:55:59 +0600

    took all add product info in a handler

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,22 +1,36 @@
 const AddProduct = () => {
+  const handleAddProduct = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const image = form.image.value;
+    const brandName = form.brandName.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const description = form.description.value;
+    const rating = form.rating.value;
+    const product = {name, image, brandName, type, price, rating, description}
+    console.log(product);
+  }
+
   return (
     <section className="py-20">
       <div className="container mx-auto">
         <h2 className="mb-5 font-bold text-3xl text-center">Add Product</h2>
         <div className="card mx-auto w-full md:w-2/3 shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleAddProduct} className="card-body">
             <div className="flex gap-5">
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="name" className="input input-bordered" />
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Image</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="image" className="input input-bordered" />
               </div>
             </div>
             <div className="flex gap-5">
@@ -24,13 +38,13 @@ const AddProduct = () => {
                 <label className="label">
                   <span className="label-text">Brand Name</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="brandName" className="input input-bordered" />
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Type</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="type" className="input input-bordered" />
               </div>
             </div>
             <div className="flex gap-5">
@@ -38,20 +52,20 @@ const AddProduct = () => {
                 <label className="label">
                   <span className="label-text">Price</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="price" className="input input-bordered" />
               </div>
               <div className="form-control w-full">
                 <label className="label">
                   <span className="label-text">Rating</span>
                 </label>
-                <input type="text" className="input input-bordered" />
+                <input type="text" name="rating" className="input input-bordered" />
               </div>
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Short Description</span>
               </label>
-              <input type="text" className="input input-bordered" />
+              <input type="text" name="description" className="input input-bordered" />
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-info">Add Product</button>
