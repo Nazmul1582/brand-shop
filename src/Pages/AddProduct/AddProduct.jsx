f70bb4f632d70248b604b33953b835f1817bacd2
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-19 07:39:00 +0600

    created add product form

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,9 +1,66 @@
 const AddProduct = () => {
-    return (
-        <div>
-            Add Product
+  return (
+    <section className="py-20">
+      <div className="container mx-auto">
+        <h2 className="mb-5 font-bold text-3xl text-center">Add Product</h2>
+        <div className="card mx-auto w-full md:w-2/3 shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="flex gap-5">
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Image</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+            </div>
+            <div className="flex gap-5">
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Brand Name</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Type</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+            </div>
+            <div className="flex gap-5">
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Rating</span>
+                </label>
+                <input type="text" className="input input-bordered" />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Short Description</span>
+              </label>
+              <input type="text" className="input input-bordered" />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-info">Add Product</button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default AddProduct;
