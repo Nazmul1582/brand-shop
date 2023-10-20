c71100829df25edaa1568b3c2ef2e348f6089d7a
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-20 10:31:24 +0600

    created a authContext and wrapped all components with it's provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Login from './Pages/Login/Login.jsx'
-import Home from './Pages/Home/Home.jsx'
-import AddProduct from './Pages/AddProduct/AddProduct.jsx'
-import MyCart from './Pages/MyCart/MyCart.jsx'
-import Products from './Pages/Products/Products.jsx'
-import ProductDetails from './Pages/ProductDetails/ProductDetails.jsx'
-import Update from './Pages/Update/Update.jsx'
-import Register from './Pages/Register/Register.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login/Login.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import AddProduct from "./Pages/AddProduct/AddProduct.jsx";
+import MyCart from "./Pages/MyCart/MyCart.jsx";
+import Products from "./Pages/Products/Products.jsx";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails.jsx";
+import Update from "./Pages/Update/Update.jsx";
+import Register from "./Pages/Register/Register.jsx";
+import AuthProvider from "./Provider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -19,46 +20,51 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "add-product",
-        element: <AddProduct />
+        element: <AddProduct />,
       },
       {
         path: "my-cart",
         element: <MyCart />,
-        loader: () => fetch('http://localhost:5000/my-cart')
+        loader: () => fetch("http://localhost:5000/my-cart"),
       },
       {
         path: "login",
-        element: <Login />
+        element: <Login />,
       },
       {
         path: "register",
-        element: <Register />
+        element: <Register />,
       },
       {
         path: "products/:brand",
         element: <Products />,
-        loader: ({params}) => fetch(`http://localhost:5000/products/${params.brand}`)
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/products/${params.brand}`),
       },
       {
         path: "product-details/:id",
         element: <ProductDetails />,
-        loader: ({params}) => fetch(`http://localhost:5000/product-details/${params.id}`)
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/product-details/${params.id}`),
       },
       {
         path: "/update/:id",
         element: <Update />,
-        loader: ({params}) => fetch(`http://localhost:5000/update/${params.id}`)
-      }
-    ]
-  }
-])
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/update/${params.id}`),
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
