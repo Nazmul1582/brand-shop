e213591ee7beb4ce3992ceb2837ce291e3e5d35e
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-20 06:46:16 +0600

    displayed users addtocart products on my-cart page

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -1,13 +1,44 @@
 import { useLoaderData } from "react-router-dom";
+import { BiDollar } from "react-icons/bi";
 
 const MyCart = () => {
-    const products = useLoaderData()
-    console.log(products);
-    return (
-        <div>
-            My Cart
+  const products = useLoaderData();
+  console.log(products);
+  return (
+    <section className="py-20">
+      <div className="container mx-auto">
+        <div className="overflow-x-auto w-2/3 mx-auto">
+          <table className="table table-zebra">
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={product.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{product.name}</td>
+                  <td>
+                    <div className="flex items-center">
+                      <BiDollar /> {product.price}
+                    </div>
+                  </td>
+                  <td>
+                    <button className="btn btn-error btn-sm">delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default MyCart;
