b595233d6c33dbc270c1ff141324d11c660dabdc
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-20 11:48:32 +0600

    set user and loading in authprovider and sent them as value

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     const createUser = async(email, password, name) => {
         await createUserWithEmailAndPassword(auth, email, password)
@@ -24,7 +36,7 @@ const AuthProvider = ({children}) => {
     }
 
 
-    const authInfo = {createUser, login, logout}
+    const authInfo = {user, loading, createUser, login, logout}
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
