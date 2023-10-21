2eed2beecc6c8802290b08e8a2405e0ac87447e4
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-21 11:00:25 +0600

    modified slider of product details page

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -8,12 +8,12 @@ const Banner = () => {
             "url(https://i.ibb.co/gVfZ3xR/blue-headphones.jpg)",
         }}
       >
-        <div className="hero-overlay bg-opacity-0"></div>
+        <div className="hero-overlay bg-opacity-10"></div>
         <div className="container mx-auto">
         <div className="hero-content justify-start">
           <div className="max-w-md">
             <p className="font-semibold">Sale Offer <span className="text-red-500">-10% Off</span> This Week</p>
-            <h2 className="my-5 text-5xl font-bold text-slate-500">Top Trending Headphone 2023</h2>
+            <h2 className="my-5 text-5xl font-bold text-info">Top Trending Headphone 2023</h2>
             <h3 className="mb-6 font-semibold">Starting at <span className="text-red-500 font-bold text-5xl">$309.00</span></h3>
             <button className="btn btn-info">Shop Now</button>
           </div>
--- src/Pages/Products/SlideContent.jsx
+++ src/Pages/Products/SlideContent.jsx
@@ -1,25 +1,31 @@
 import PropTypes from 'prop-types'
 
-const SlideContent = ({productName, price}) => {
+const SlideContent = ({image, productName, price}) => {
     return (
-          <div className="max-w-md absolute top-5 md:top-14 left-10 md:left-16">
-            <p className="font-semibold">
-              Sale Offer <span className="text-red-500">-10% Off</span> This
-              Week
-            </p>
-            <h2 className="mt-5 mb-6 text-5xl font-bold text-slate-500">
-              Top Trending {productName} 2023
-            </h2>
-            <h3 className="mb-5 font-semibold">
-              Starting at{" "}
-              <span className="text-red-500 font-bold text-5xl">${price}</span>
-            </h3>
-            <button className="btn btn-info">Shop Now</button>
-          </div>
+      <div
+      className="hero min-h-[80vh] bg-cover"
+      style={{
+        backgroundImage:
+          `url(${image})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-10"></div>
+      <div className="container mx-auto">
+      <div className="hero-content justify-start">
+        <div className="max-w-md">
+          <p className="font-semibold">Sale Offer <span className="text-red-500">-10% Off</span> This Week</p>
+          <h2 className="my-5 text-5xl font-bold text-info">Top Trending {productName} 2023</h2>
+          <h3 className="mb-6 font-semibold">Starting at <span className="text-red-500 font-bold text-5xl">${price}</span></h3>
+          <button className="btn btn-info">Shop Now</button>
+        </div>
+      </div>
+      </div>
+    </div>
     );
 };
 
 SlideContent.propTypes = {
+  image: PropTypes.string,
   productName: PropTypes.string,
   price: PropTypes.number
 }
